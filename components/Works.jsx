a870aa244aabdbd4dcfc46333f9fb1598831530c
Sijan666 <666majharulislam@gmail.com>
2026-04-19 17:28:18 +0600

    chnages in works common component

--- components/Works.jsx
+++ components/Works.jsx
@@ -7,7 +7,7 @@ export default function Works({className , title , type}) {
         <>
         <div className={`${className} group `}>
             <div className="lg:h-105 h-50 lg:w-142.5 bg-[#C4C4C4] hover:bg-secondarys/60 duration-300">
-                <button className='p-6.25 bg-white text-primarys rounded-full mt-5 lg:ml-120 opacity-0 group-hover:opacity-100 duration-300 cursor-pointer'><FaArrowRight size={20}/></button>
+                <button className='p-6.25 bg-white text-primarys rounded-full mt-5 lg:ml-120 ml-20 opacity-0 group-hover:opacity-100 duration-300 cursor-pointer'><FaArrowRight size={20}/></button>
             </div>
             <div className="lg:h-25 lg:w-142.5 bg-secondarys py-5.5 px-4.5">
                 <h4 className='text-white font-vol text-[26px] font-semibold'>{title}</h4>
