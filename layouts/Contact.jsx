a04a985aedfb35c0cb63539274019902daca7035
Sijan666 <666majharulislam@gmail.com>
2026-04-26 23:55:17 +0600

    changes in contact

--- layouts/Contact.jsx
+++ layouts/Contact.jsx
@@ -16,14 +16,14 @@ export default function Contact() {
                         </div>
                         <div className="right">
                             {/* heading */}
-                            <div className="lg:w-52 mx-auto flex items-center gap-x-2.25">
+                            <div className="lg:w-52 flex items-center gap-x-2.25">
                                 <div className="flex items-center">
                                     <div className="w-10 h-px bg-primarys"></div>
                                     <div className="w-1.5 h-1.5 bg-primarys rounded-full -ml-1"></div>
                                 </div>
                                 <h4 className='text-primarys text-lg font-medium font-vol text-center tracking-[2%]'>Get In Touch</h4>
-                                <h3 className='text-center text-[40px] lg:text-[50px] text-secondarys font-medium font-vol lg:px-75 pt-2.75 leading-15'>Feedback From Client</h3>
                             </div>
+                            <h3 className='text-center text-[40px] lg:text-[50px] text-secondarys font-medium font-vol  pt-2.75 leading-15'>Feel Free to Contact</h3>
                         </div>
                     </div>
                 </Container>
