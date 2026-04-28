60ab79a1fd4f07740eb1c41dad2df33e75a10f2d
Sijan666 <666majharulislam@gmail.com>
2026-04-28 23:32:45 +0600

    add social icons in footer

--- layouts/Footer.jsx
+++ layouts/Footer.jsx
@@ -2,6 +2,8 @@ import Container from '@/components/Container'
 import Image from 'next/image'
 import React from 'react'
 import footerLogo from '../public/footerLogo.png'
+import { TiSocialSkypeOutline } from 'react-icons/ti'
+import { FaBehance, FaInstagram } from 'react-icons/fa6'
 
 
 export default function Footer() {
@@ -26,6 +28,11 @@ export default function Footer() {
                                 <div className="text-white/10 w-0.5 h-6.5">|</div>
                                 <button className='text-white cursor-pointer'>Contract</button>
                             </div>
+                        </div>
+                        <div className="mt-12.5 flex justify-center gap-x-5 items-center">
+                            <TiSocialSkypeOutline size={60} className='p-3.25 bg-primarys rounded-full text-white cursor-pointer' />
+                            <FaInstagram  size={60} className='p-3.25 bg-primarys rounded-full text-white cursor-pointer' />
+                            <FaBehance  size={60} className='p-3.25 bg-primarys rounded-full text-white cursor-pointer' />
                         </div>
                     </div>
                 </Container>
